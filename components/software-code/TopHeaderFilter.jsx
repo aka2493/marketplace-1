0f568ace51aaa10f9a6ac229a63a9348e62e4090
stebr-payal <payal@stebr.in>
2023-04-13 00:36:32 +0530

    removed commented code

--- components/software-code/TopHeaderFilter.jsx
+++ components/software-code/TopHeaderFilter.jsx
@@ -27,35 +27,6 @@ const TopHeaderFilter = () => {
                         </button>
                     </div>
                 </div>
-
-                {/* <div className="col-auto">
-                    <div className="row x-gap-20 y-gap-20">
-                        <div className="col-auto d-flex items-center">
-                            <button className="button -blue-1 h-40 px-20 rounded-100 bg-blue-1-05 text-15 text-blue-1">
-                                <i className="icon-up-down text-14 mr-10" />
-                                Sort
-                            </button>
-                            <div className="text-16 fw-500 mr-10">Sort By:</div>
-                            <select placeholder="Sort By" className="form-select rounded-4 border-light justify-between text-15 fw-500 px-20 h-34 w-180 sm:w-full text-14">
-                                <option value="newset">Newest</option>
-                                <option value="design">Price Low to High</option>
-                                <option value="illustration">Price High to Low</option>
-                            </select>
-                        </div>
-
-                        <div className="col-auto d-none xl:d-block">
-                            <button
-                                data-bs-toggle="offcanvas"
-                                data-bs-target="#listingSidebar"
-                                className="button -blue-1 h-40 px-20 rounded-100 bg-blue-1-05 text-15 text-blue-1"
-                            >
-                                <i className="icon-up-down text-14 mr-10" />
-                                Filter
-                            </button>
-                        </div>
-                    </div>
-                </div> */}
-
             </div>
         </>
     );
