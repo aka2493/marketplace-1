063c99530934ba7ce9647f3f3b48a80624dd3969
aka2493 <akanksha@stebr.com>
2023-04-21 11:37:06 -0700

    Did changes in pricing in ecommerce

--- components/startSelling/addEcommerceProductDetails/Pricing.js
+++ components/startSelling/addEcommerceProductDetails/Pricing.js
@@ -4,6 +4,8 @@ import { Checkbox } from "@nextui-org/react";
 
 const Pricing = () => {
   const [selected, setSelected] = useState(false);
+  const [subscriptionselect, subscriptionsetSelected] = useState(false);
+
   return (
     <>
       <div className="col-xl-8 col-lg-8 mt-30">
@@ -12,7 +14,39 @@ const Pricing = () => {
           <span className="text-20 fw-400">/4</span>
         </div>
         <div className="row x-gap-20 y-gap-20 pt-10">
-          <h2 className="text-20 fw-500 mt-10 mb-10">Subscription</h2>
+        <h2 className="text-20 fw-500 mt-10 mb-10">Regular Pricing</h2>
+        <div
+        className="mt-10 mb-20 price"
+        style={{ gap: "20px", display: "flex" }}
+      >
+        <div className="col-md-6">
+          <h2 className="text-18 fw-500">Actual Price</h2>
+          <div className="form-input mt-10">
+            <input type="text" required />
+          </div>
+        </div>
+
+        <div className="col-md-6">
+          <h2 className="text-18 fw-500">Discount Price&nbsp;(optional)</h2>
+          <div className="form-input mt-10">
+            <input type="text" required />
+          </div>
+        </div>
+      </div>
+<div className="mb-10">
+        <Checkbox
+        isSelected={subscriptionselect}
+        color="success"
+        onChange={subscriptionsetSelected}
+      >
+        <label className="text-18 fw-500">
+         Do you have Subscription Plans?
+        </label>
+      </Checkbox>
+      </div>
+      {subscriptionselect &&(
+        <div>
+          <h2 className="text-20 fw-500 mt-10 mb-30">Subscription</h2>
           <div className="col-md-12">
             <h2 className="text-18 fw-500 mb-20">Select Subscription Plans</h2>
             <div className="dropdown col-md-6 js-dropdown js-price-1-active">
@@ -47,12 +81,14 @@ const Pricing = () => {
               </div>
             </div>
           </div>
-
-          <div className="pt-10 mt-30 border-top-light" />
+  
+          <div className="pt-10 mt-40 border-top-light" />
           {/* border separation */}
 
+          
+          </div>  )}
           <div className="col-md-12">
-            <div className="mt-30">
+       
               <Checkbox
                 isSelected={selected}
                 color="success"
@@ -62,7 +98,7 @@ const Pricing = () => {
                   Are you offering a deal?
                 </label>
               </Checkbox>
-            </div>
+        
           </div>
           {/* End col-12 */}
           {selected && (
